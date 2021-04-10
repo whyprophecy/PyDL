faf34458b4c14052e32cd546540fed85d5dc74da
whyprophecy <hengyuewang72@gmail.com>
2021-04-10 22:44:47 +0800

    change no.2

--- test2.py
+++ test2.py
@@ -17,4 +17,8 @@ obj = item
 researchcontent = 'a pony'
 researchresult = requests.get(
     'https://bindingofisaacrebirth.fandom.com/api.php?action=query&list=search&format=json&srsearch=%s&srlimit=1' % researchcontent)
-
+obj.name = json.loads(researchresult.text)['query']['search'][0]['title']
+maincontent = requests.get(
+    'https://bindingofisaacrebirth.fandom.com/api.php?action=query&prop=revisions&format=json&titles=%s&rvprop=content' % obj.name)
+js = json.loads(maincontent.text)
+print(js['query']['pages']['2856']['revisions'][0]['*'])
