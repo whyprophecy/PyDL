a66c505cc9ab8e34dba2c1868d17d323bf1a0811
whyprophecy <hengyuewang72@gmail.com>
2021-04-11 00:07:19 +0800

    complete the API part of the program

--- test2.py
+++ test2.py
@@ -15,7 +15,7 @@ class item:
 
 
 obj = item
-researchcontent = 'blank card'
+researchcontent = 'god tears'
 researchresult = requests.get(
     'https://bindingofisaacrebirth.fandom.com/api.php?action=query&list=search&format=json&srsearch=%s&srlimit=1' % researchcontent)
 js = json.loads(researchresult.text)
@@ -24,8 +24,11 @@ obj.id = js['query']['search'][0]['pageid']
 maincontent = requests.get(
     'https://bindingofisaacrebirth.fandom.com/api.php?action=query&prop=revisions&format=json&titles=%s&rvprop=content' % obj.name)
 js = json.loads(maincontent.text)
-#print(js['query']['pages']['%d' % obj.id]['revisions'][0]['*'])
-imagelist = requests.get(
+contentlist = js['query']['pages']['%d' %
+                                   obj.id]['revisions'][0]['*'].split('\n')[1:4]
+for each in contentlist:
+    print(each.split('= ')[1].replace('[[','').replace(']]',''))
+'''imagelist = requests.get(
     'https://bindingofisaacrebirth.fandom.com/api.php?action=query&prop=images&format=json&titles=%s' % obj.name)
 js = json.loads(imagelist.text)
 for each in js['query']['pages']['%d' % obj.id]['images']:
@@ -36,5 +39,4 @@ imageinfo = requests.get(
 js = json.loads(imageinfo.text)
 for imageid in js['query']['pages']:
     pass
-obj.imageurl = js['query']['pages']['%s' % imageid]['imageinfo'][0]['url']
-print(obj.imageurl)
+obj.imageurl = js['query']['pages']['%s' % imageid]['imageinfo'][0]['url']'''
