02e09432ddcd007488bd7ad4614b1785f93674d1
whyprophecy <hengyuewang72@gmail.com>
2021-04-11 14:51:47 +0800

    tweak some part

--- test3.py
+++ test3.py
@@ -15,10 +15,13 @@ class searchitem(QWidget):
         self.setWindowTitle('Issac Item Searcher')
 
         self.headimage = QLabel(self)
-        headimage = QPixmap('isaac.png')
+        Image.open(requests.get('https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/e/e5/Character_Isaac_appearance.png/revision/latest/scale-to-width-down/84?cb=20150104210047', stream=True).raw).save(
+            'temporaryimage1.png')
+        headimage = QPixmap('temporaryimage1.png')
         self.headimage.setPixmap(headimage)
 
         self.itemimage = QLabel('ItemImage', self)
+        self.itemname = QLabel(self.item.name, self)
         self.itemid = QLabel(self.item.itemid, self)
         self.itemquote = QLabel(self.item.quote, self)
         self.itemdescription = QLabel(self.item.description, self)
@@ -30,29 +33,31 @@ class searchitem(QWidget):
         self.searchcontent.setFocus()
 
         vboxleft = QVBoxLayout()
-        vboxleft.addStretch(1)
+        vboxleft.addStretch(2)
         vboxleft.addWidget(self.headimage)
         vboxleft.addStretch(1)
         vboxleft.addWidget(self.searchcontent)
         vboxleft.addStretch(1)
         vboxleft.addWidget(self.searchbutton)
-        vboxleft.addStretch(1)
+        vboxleft.addStretch(2)
 
         vboxright = QVBoxLayout()
-        vboxright.addStretch(1)
+        vboxright.addStretch(2)
         vboxright.addWidget(self.itemimage)
+        vboxright.addStretch(1)
+        vboxright.addWidget(self.itemname)
         vboxright.addStretch(1)
         vboxright.addWidget(self.itemid)
         vboxright.addStretch(1)
         vboxright.addWidget(self.itemquote)
         vboxright.addStretch(1)
         vboxright.addWidget(self.itemdescription)
-        vboxright.addStretch(1)
+        vboxright.addStretch(2)
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addLayout(vboxleft)
-        hbox.addStretch(1)
+        hbox.addStretch(2)
         hbox.addLayout(vboxright)
         hbox.addStretch(1)
 
@@ -64,12 +69,13 @@ class searchitem(QWidget):
 
     def refresh(self):
         Image.open(requests.get(self.item.imageurl, stream=True).raw).save(
-            'temporaryimage.png')
-        itemimage = QPixmap('temporaryimage.png')
+            'temporaryimage2.png')
+        itemimage = QPixmap('temporaryimage2.png')
         self.itemimage.setPixmap(itemimage)
-        self.itemid.setText(self.item.itemid)
-        self.itemquote.setText(self.item.quote)
-        self.itemdescription.setText(self.item.description)
+        self.itemname.setText('Name: '+self.item.name)
+        self.itemid.setText('ID: '+self.item.itemid)
+        self.itemquote.setText('Quote: '+self.item.quote)
+        self.itemdescription.setText('Description:\n'+self.item.description)
 
 
 if __name__ == '__main__':
